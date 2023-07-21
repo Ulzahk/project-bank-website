c40b9d1f6db56e71af3922a1bdb085ec8cc4653e
Ulzahk <ulzahk@gmail.com>
2023-07-21 05:00:47 -0500

    feat(About): update content and add new bank name

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Layout from '@components/Layout';
-import AboutImage1 from '@images/about-image-1.png';
+import AboutPartnership from '@images/about-partnership.jpg'
+import AboutCustomerFocus from '@images/about-customer-focus.jpg';
+import AboutCommunity from '@images/about-community.jpg';
 import Image from 'next/image'
 import styles from '@styles/about.module.scss';
 
@@ -14,7 +16,7 @@ function About(){
         <div className={styles.AboutCardImageWrapper}>
           <Image 
             alt='group of 4 persons, two of them shaking hands'
-            src={AboutImage1.src}
+            src={AboutPartnership.src}
             className={styles.AboutCardImage}
             width={640}
             height={427}
@@ -22,22 +24,24 @@ function About(){
         </div>
         <div className={styles.AboutCardTextWrapper}>
           <h2 className={styles.AboutCardTextTitle}>Your Trusted Financial Partner</h2>
-          <p>Welcome to [Bank Name], where your financial aspirations come to life.</p>
-          <p>At [Bank Name], we understand that your financial well-being is of utmost importance. With over 10 years of experience in the industry, we have built a strong foundation based on trust, reliability, and personalized service. We take pride in being your trusted partner in achieving your financial goals.</p>
+          <p>Welcome to Nexus Unity Bank, where your financial aspirations come to life.</p>
+          <p>At Nexus Unity Bank, we understand that your financial well-being is of utmost importance. With over 10 years of experience in the industry, we have built a strong foundation based on trust, reliability, and personalized service. We take pride in being your trusted partner in achieving your financial goals.</p>
           <p>Our mission is to empower individuals, families, and businesses with the tools and resources they need to thrive. Whether you are a first-time account holder, a seasoned investor, or a business owner, we offer a wide range of innovative financial solutions tailored to meet your unique needs.</p>
         </div>
       </div>
       <div className={`${styles.AboutCard} ${styles.DarkBackground}`}>
         <div className={styles.AboutCardTextWrapper}>
-          <h2 className={styles.AboutCardTextTitle}>Your Trusted Financial Partner</h2>
-          <p>Welcome to [Bank Name], where your financial aspirations come to life.</p>
-          <p>At [Bank Name], we understand that your financial well-being is of utmost importance. With over [number of years] years of experience in the industry, we have built a strong foundation based on trust, reliability, and personalized service. We take pride in being your trusted partner in achieving your financial goals.</p>
-          <p>Our mission is to empower individuals, families, and businesses with the tools and resources they need to thrive. Whether you are a first-time account holder, a seasoned investor, or a business owner, we offer a wide range of innovative financial solutions tailored to meet your unique needs.</p>
+          <h2 className={styles.AboutCardTextTitle}>Why Choose Nexus Unity Bank? Here Is What Sets Us Apart</h2>
+          <ul>
+            <li><b>Integrity:</b> Trust is the cornerstone of everything we do. We conduct our business with unwavering integrity, transparency, and honesty. You can rely on us to always act in your best interests.</li>
+            <li><b>Customer Focus:</b> You are at the heart of our organization. We listen, understand your needs, and provide tailored solutions to help you achieve your financial goals. Your success is our success.</li>
+            <li><b>Excellence:</b> Striving for excellence is ingrained in our culture. We continually challenge ourselves to deliver superior banking services, innovative products, and an exceptional customer experience.</li>
+          </ul>
         </div>
         <div className={styles.AboutCardImageWrapper}>
           <Image 
-            alt='group of 4 persons, two of them shaking hands'
-            src={AboutImage1.src}
+            alt='two women sitting chatting, one of them holding a gray laptop'
+            src={AboutCustomerFocus.src}
             className={styles.AboutCardImage}
             width={640}
             height={427}
@@ -47,18 +51,17 @@ function About(){
       <div className={styles.AboutCard}>
         <div className={styles.AboutCardImageWrapper}>
           <Image 
-            alt='group of 4 persons, two of them shaking hands'
-            src={AboutImage1.src}
+            alt='group of persons sitting at the park in a sunny day'
+            src={AboutCommunity.src}
             className={styles.AboutCardImage}
             width={640}
             height={427}
           />
         </div>
         <div className={styles.AboutCardTextWrapper}>
-          <h2 className={styles.AboutCardTextTitle}>Your Trusted Financial Partner</h2>
-          <p>Welcome to [Bank Name], where your financial aspirations come to life.</p>
-          <p>At [Bank Name], we understand that your financial well-being is of utmost importance. With over [number of years] years of experience in the industry, we have built a strong foundation based on trust, reliability, and personalized service. We take pride in being your trusted partner in achieving your financial goals.</p>
-          <p>Our mission is to empower individuals, families, and businesses with the tools and resources they need to thrive. Whether you are a first-time account holder, a seasoned investor, or a business owner, we offer a wide range of innovative financial solutions tailored to meet your unique needs.</p>
+          <h2 className={styles.AboutCardTextTitle}>Community Commitment</h2>
+          <p>We are deeply rooted in the communities we serve. Through various corporate social responsibility initiatives, we actively give back and contribute to the betterment of society. Together, we can make a positive impact on the lives of individuals and the community as a whole.</p>
+          <p>At Nexus Unity Bank, your trust and satisfaction are our top priorities. We invite you to explore our website, visit our branches, or contact our dedicated customer service team to experience the Nexus Unity Bank difference firsthand.</p>
         </div>
       </div>
     </section>
